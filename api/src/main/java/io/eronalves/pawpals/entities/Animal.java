19ef14ed22e3544b18bb1a6da2ec4798bbe67596
Eron Alves <eron.a.silva@capgemini.com>
2022-12-20 01:47:03 -0300

    fix: fix constructors and id type on models

--- api/src/main/java/io/eronalves/pawpals/entities/Animal.java
+++ api/src/main/java/io/eronalves/pawpals/entities/Animal.java
@@ -3,19 +3,24 @@ package io.eronalves.pawpals.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Animal {
 
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
     @NotNull
     @Size(max = 30)
--- api/src/main/java/io/eronalves/pawpals/entities/User.java
+++ api/src/main/java/io/eronalves/pawpals/entities/User.java
@@ -2,20 +2,25 @@ package io.eronalves.pawpals.entities;
 
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
     @NotNull
     @Size(max = 30)
