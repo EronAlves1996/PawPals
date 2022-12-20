c476b4c3372f97020dee752a27db8f6381872d7a
Eron Alves <eron.a.silva@capgemini.com>
2022-12-20 11:40:52 -0300

    feat: expose endpoint for retire interest in adoption

--- api/src/main/java/io/eronalves/pawpals/controllers/UserController.java
+++ api/src/main/java/io/eronalves/pawpals/controllers/UserController.java
@@ -1,10 +1,16 @@
 package io.eronalves.pawpals.controllers;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
 
 import io.eronalves.pawpals.entities.AnimalPreferences;
 import io.eronalves.pawpals.entities.User;
@@ -31,6 +37,18 @@ public class UserController {
                 preferences.getSize(), preferences.getType(), id);
 
         return userRepository.findById(id).get();
+    }
+
+    @PutMapping("/no-adopt/{id}")
+    public User retireInterestToAdopt(@PathVariable("id") int id) {
+        Optional<User> userOpt = userRepository.findById(id);
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            user.setPreferences(null);
+            user.setLookingForAnimal(false);
+            return userRepository.save(user);
+        }
+        throw new ErrorResponseException(HttpStatus.NOT_FOUND);
     }
 
 }
