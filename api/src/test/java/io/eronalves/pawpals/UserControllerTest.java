b52d31a6ef0edd6763e1516a96169c530d9000c0
Eron Alves <eron.a.silva@capgemini.com>
2022-12-20 02:14:59 -0300

    test: write test case for adoptions preferences

--- api/src/test/java/io/eronalves/pawpals/UserControllerTest.java
+++ api/src/test/java/io/eronalves/pawpals/UserControllerTest.java
@@ -1,18 +1,19 @@
 package io.eronalves.pawpals;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import javax.print.attribute.standard.Media;
-import javax.swing.plaf.metal.MetalBorders.MenuItemBorder;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,6 +21,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.eronalves.pawpals.entities.AnimalPreferences;
+import io.eronalves.pawpals.entities.AnimalSize;
+import io.eronalves.pawpals.entities.AnimalType;
 import io.eronalves.pawpals.entities.User;
 import io.eronalves.pawpals.repositories.UserRepository;
 
@@ -37,7 +41,7 @@ class UserControllerTest {
 	}
 
 	@Test
-	public void registerUser() throws JsonProcessingException, Exception {
+	public void testRegisterUser() throws JsonProcessingException, Exception {
 		User user = new User(1, "Eron", "eron@eron.com", false, null);
 		when(userRepository.save(any())).thenReturn(user);
 
@@ -45,6 +49,24 @@ class UserControllerTest {
 		mockMvc
 				.perform(post("/users/register")
 						.content(om.writeValueAsString(new User(null, "Eron", "eron@eron.com", false, null)))
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().json(om.writeValueAsString(user)));
+	}
+
+	@Test
+	public void restUpdateAdoptionPreferences() throws JsonProcessingException, Exception {
+		AnimalPreferences preferences = new AnimalPreferences("black", false, AnimalSize.SMALL, AnimalType.DOG);
+		User user = new User(1, "Eron", "eron@eron.com", true,
+				preferences);
+		when(userRepository.findById(1))
+				.thenReturn(Optional.of(user));
+
+		ObjectMapper om = new ObjectMapper();
+
+		mockMvc
+				.perform(post("/users/1/adoption")
+						.content(om.writeValueAsString(preferences))
 						.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().json(om.writeValueAsString(user)));
